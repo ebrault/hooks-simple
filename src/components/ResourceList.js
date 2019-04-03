7abfa8e828641690698b0db730cfe64ada5a7b43
ebrault <emmet.brault@gmail.com>
2019-04-03 12:34:40 -0400

    Created useResources component
    Removed useResources from ResourceList into its own component
    - Imported useState, useEffect from react
    - Imported axios
    - Copied and pasted useResources code from ResourceList
    - Exported useResources
    - Imported useResources into ResourceList so that it can still be called
    - Tested successfully

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,21 +1,5 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-
-const useResources = (resource) => {
-  const [resources, setResources] = useState([]);
-
-  useEffect(() => {
-    (async (resource) => {
-      const response = await axios.get(
-        `https://jsonplaceholder.typicode.com/${resource}`
-      )
-      setResources(response.data)
-    })(resource)
-  }, [resource])
-
-  return resources;
-}
+import useResources from './useResources';
 
 const ResourceList = ({ resource }) => {
   const resources = useResources(resource)
