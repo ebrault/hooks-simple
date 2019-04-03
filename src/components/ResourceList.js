86716189a85e3e006d6674c272377c4fc24e9855
ebrault <emmet.brault@gmail.com>
2019-04-03 12:30:28 -0400

    Refactored ResourceList component
    Refactored ResourceList component for higher code reusability
    
    - Pulled hook functions out into their own function called useResources
    - Created variable resources in ResourceList and assigned it to a
      function call of useResources with resource passed in
    - This allows us to pass in any resource that exists on the endpoint to
      useResources

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -2,22 +2,33 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ResourceList = ({ resource }) => {
-
+const useResources = (resource) => {
   const [resources, setResources] = useState([]);
 
-  const fetchResource = async (resource) => {
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/${resource}`
-    )
-    setResources(response.data)
-  }
-
   useEffect(() => {
-    fetchResource(resource)
+    (async (resource) => {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/${resource}`
+      )
+      setResources(response.data)
+    })(resource)
   }, [resource])
 
-  return <div>{resources.length}</div>
+  return resources;
+}
+
+const ResourceList = ({ resource }) => {
+  const resources = useResources(resource)
+  return (
+    <ul>
+      {resources.map(
+        record =>
+          <li key={record.id}>
+            {record.title}
+          </li>
+        )}
+      </ul>
+    );
 }
 
 export default ResourceList
